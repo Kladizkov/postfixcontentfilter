2ea167cdb9144be0c88f6540fa8913ebbd175f0f
Kladizkov <kladizkov.theone@gmail.com>
2025-04-29 14:50:50 +0530

    Add files via upload
    for python 3

--- pfcf.py
+++ pfcf.py
@@ -1,174 +1,134 @@
-import smtpd
-import asyncore
-import datetime
+import asyncio
+from aiosmtpd.controller import Controller
+from aiosmtpd.handlers import AsyncMessage
+from email.message import EmailMessage
 import smtplib
 import logging
 import traceback
 import syslog
 import systemd.daemon
 import configparser
+import datetime
 
-class CustomSMTPServer(smtpd.SMTPServer):
 
-	def process_message(self, peer, mailfrom, rcpttos, data):
-		
-		mailfrom.replace('\'', '')
-		mailfrom.replace('\"', '')
-		
-		for recipient in rcpttos:
-			recipient.replace('\'', '')
-			recipient.replace('\"', '')
-		
-		logging.info('Content Filter Started')
+class CustomSMTPHandler(AsyncMessage):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.load_config()
 
-		logging.info('Message addressed from: %s', mailfrom)
-		logging.info('Message addressed to : %s', ', '.join(rcpttos))
+    def load_config(self):
+        self.companyName = self.config.get('default', 'CompanyName')
+        self.adminEmail = self.config.get('default', 'AdminEmail')
+        self.primarySenderDomainToFilter = self.config.get('default', 'PrimarySenderDomainToFilter')
+        self.addressesAllowedForOutbound = self.config.get('default', 'AddressesAllowedForOutbound').split(',')
+        self.allowedOutboundDomains = self.config.get('default', 'AllowedOutboundDomains').split(',')
+        self.bounceSender = self.config.get('default', 'BounceSender')
+        logFile = self.config.get('default', 'LogFile')
 
-		try:
+        logging.basicConfig(
+            level=logging.INFO,
+            filename=logFile,
+            filemode='a',
+            format='%(asctime)s :: %(levelname)s :: %(message)s'
+        )
 
-			x = datetime.datetime.now()
-			bounceMailContent = """From: Mailer <""" + bounceSender + """>
-To: """ + mailfrom + """
+    async def handle_message(self, message: EmailMessage):
+        mailfrom = message['X-MailFrom'].replace("'", "").replace('"', '')
+        xrcpttos = []
+        xrcpttos.extend(message.get_all('X-RcptTo', []))
+        xrcpttos = [addr.strip() for part in xrcpttos for addr in part.split(';')]
+        xrcpttos = [addr.strip() for part in xrcpttos for addr in part.split(',')]
+        
+        logging.info("Content Filter Started")
+        logging.info("Message addressed from: %s", mailfrom)
+        logging.info("Message addressed to (X-RcptTo): %s", ', '.join(xrcpttos)) if xrcpttos else logging.info("No X-RcptTo recipients")
+
+        mailToFilter = 0
+        mailRejected = 0
+        xrcpttos_filtered = []
+
+        try:
+            if self.primarySenderDomainToFilter in mailfrom:
+                if mailfrom in self.addressesAllowedForOutbound:
+                    mailToFilter = 0
+                    syslog.syslog('pfcf: Exempted sender found. Skipping filter.')
+                    logging.info("Exempted sender found. Skipping filter.")
+                else:
+                    mailToFilter = 1
+                    syslog.syslog('pfcf: Unprivileged sender. Filtering.')
+                    logging.info("Unprivileged sender. Filtering.")
+                    for rcptTo in xrcpttos:
+                        if any(domain in rcptTo for domain in self.allowedOutboundDomains):
+                            xrcpttos_filtered.append(rcptTo)
+                    if len(xrcpttos) != len(xrcpttos_filtered):
+                        mailRejected = 1
+                        syslog.syslog('pfcf: All or some recipient rejected.')
+                        logging.info("=== REJECTED MESSAGE START ===\n%s\n=== REJECTED MESSAGE END ===", message)
+            else:
+                mailToFilter = 0
+                syslog.syslog('pfcf: No primary sender domain. Skipping filter.')
+                logging.info("No primary sender domain. Skipping filter.")
+        except Exception:
+            syslog.syslog('pfcf: Critical Exception')
+            logging.error('Critical Exception')
+            logging.error(traceback.format_exc())
+
+        try:
+            send_to = xrcpttos_filtered if (mailToFilter == 1 and xrcpttos_filtered) else xrcpttos
+            logging.info("Allowed recipients: %s", ', '.join(send_to))
+            if send_to:
+               with smtplib.SMTP('localhost', 10026) as server:
+                server.send_message(message, from_addr=mailfrom, to_addrs=send_to)
+                logging.info("Send successful")
+
+            if mailRejected:
+                x = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S +0530")
+                bounce_msg = f"""\
+From: Mailer <{self.bounceSender}>
+To: {mailfrom}
 MIME-Version: 1.0
 Content-type: text/html
 Subject: Email Rejected
-Date: """ + x.strftime("%a, %-d %b %Y %H:%M:%S +0530") + """
+Date: {x}
 
-Your mail could not be send to one or more of its recipient as it violates """ + companyName + """ email policies.
-
+Your mail could not be sent to one or more recipients as it violates {self.companyName} email policies.
 """
-
-			bounceMailContentForAdmin = """From: Mailer <""" + bounceSender + """>
-To: """ + adminEmail + """
+                bounce_admin_msg = f"""\
+From: Mailer <{self.bounceSender}>
+To: {self.adminEmail}
 MIME-Version: 1.0
 Content-type: text/html
 Subject: Email Rejected - Policy Violation
-Date: """ + x.strftime("%a, %-d %b %Y %H:%M:%S +0530") + """
+Date: {x}
 
-EMail from """ + mailfrom + """ could not be send to one or more of its recipient as it violates """ + companyName + """ email policies. Check log for more details.
-
+Email from {mailfrom} could not be sent to one or more recipients as it violates {self.companyName} email policies. Check logs for more details.
 """
+                for recipient, content in [(mailfrom, bounce_msg), (self.adminEmail, bounce_admin_msg)]:
+                    with smtplib.SMTP('localhost', 10026) as server:
+                        server.sendmail(self.bounceSender, recipient, content)
+                logging.info("One or more messages rejected")
 
-			mailToFilter = 0
-			mailRejected = 0
-			rcpttos_filtered = []
+        except Exception as e:
+            syslog.syslog(f'pfcf: SMTP send exception - {str(e)}')
+            logging.error("SMTP send exception")
+            logging.error(traceback.format_exc())
 
-			if primarySenderDomainToFilter in mailfrom:
-				if mailfrom in addressesAllowedForOutbound:
-					mailToFilter = 0
-					syslog.syslog('pfcf: Exempted sender found in from address. Skipping filter.')
-					logging.info("Exempted sender found in from address. Skipping filter.")
-				else:
-					mailToFilter = 1
-					syslog.syslog('pfcf: Unprivileged sender found in from address. Starting filter.')
-					logging.info("Unprivileged sender found in from address. Starting filter.")
-					for rcptTo in rcpttos:
-						for allowedOutboundDomain in allowedOutboundDomains:
-							if allowedOutboundDomain in rcptTo:
-								rcpttos_filtered.append(rcptTo)
 
-					if ((len(rcpttos) != len(rcpttos_filtered))):
-						mailRejected = 1
-						syslog.syslog('pfcf: All or some of the recipient rejected.')	
-						logging.info('=== REJECTED MESSAGE START ===')
-						logging.info(data)
-						logging.info('=== REJECTED MESSAGE END ===')
-						logging.info("All or some of the recipient rejected.")
-			else:
-				mailToFilter = 0
-				syslog.syslog('pfcf: No primary sender domain found in from address. Skipping filter.')
-				logging.info("No primary sender domain found in from address. Skipping filter.")
+if __name__ == '__main__':
+    systemd.daemon.notify('READY=1')
+    syslog.syslog('pfcf: Started')
+    syslog.syslog('pfcf: Reading configuration settings')
 
-		except:
-			pass
-			syslog.syslog('pfcf: Critical Exception')
-			logging.error('Critical Exception')
-			logging.error(traceback.format_exc())
+    config = configparser.ConfigParser()
+    config.read('/etc/pfcf.ini')
 
-		try:
-			if (mailToFilter == 1):
-				if(rcpttos_filtered):
-					server = smtplib.SMTP('localhost', 10026)
-					server.sendmail(mailfrom, rcpttos_filtered, data)
-					server.quit()
-					logging.info('Send successful')
-			else:
-				server = smtplib.SMTP('localhost', 10026)
-				server.sendmail(mailfrom, rcpttos, data)
-				server.quit()
-				logging.info('Send successful')
-			
-			if mailRejected == 1:
-				server = smtplib.SMTP('localhost', 10026)
-				server.sendmail(bounceSender, mailfrom, bounceMailContent)
-				server.quit()
-				server = smtplib.SMTP('localhost', 10026)
-				server.sendmail(bounceSender, adminEmail, bounceMailContentForAdmin)
-				server.quit()
-				logging.info('One or more message rejected')
-            
-		except smtplib.SMTPException:
-			syslog.syslog('pfcf: Exception SMTPException')
-			logging.error('Exception SMTPException')
-			logging.error(traceback.format_exc())
-			pass
-		except smtplib.SMTPServerDisconnected:
-			syslog.syslog('pfcf: Exception SMTPServerDisconnected')
-			logging.error('Exception SMTPServerDisconnected')
-			pass
-		except smtplib.SMTPResponseException:
-			syslog.syslog('pfcf: Exception SMTPResponseException')
-			logging.error('Exception SMTPResponseException')
-			pass		
-		except smtplib.SMTPSenderRefused:
-			syslog.syslog('pfcf: Exception SMTPSenderRefused')
-			logging.error('Exception SMTPSenderRefused')
-			pass		
-		except smtplib.SMTPRecipientsRefused:
-			syslog.syslog('pfcf: Exception SMTPRecipientsRefused')
-			logging.error('Exception SMTPRecipientsRefused')
-			pass		
-		except smtplib.SMTPDataError:
-			syslog.syslog('pfcf: Exception SMTPDataError')
-			logging.error('Exception SMTPDataError')
-			pass		
-		except smtplib.SMTPConnectError:
-			syslog.syslog('pfcf: Exception SMTPConnectError')
-			logging.error('Exception SMTPConnectError')
-			pass		
-		except smtplib.SMTPHeloError:
-			syslog.syslog('pfcf: Exception SMTPHeloError')
-			logging.error('Exception SMTPHeloError')
-			pass		
-		except smtplib.SMTPAuthenticationError:
-			syslog.syslog('pfcf: SMTPAuthenticationError')
-			logging.error('Exception SMTPAuthenticationError')
-			pass
-		except:
-			syslog.syslog('pfcf: Undefined exception')
-			logging.error('Undefined exception')
-			logging.error(traceback.format_exc())
-
-		return
-		
-systemd.daemon.notify('READY=1')
-syslog.syslog('pfcf: Started')
-syslog.syslog('pfcf: Reading configuration settings')
-
-config = configparser.ConfigParser()
-config.read('/etc/pfcf.ini')
-companyName = config.get('default', 'CompanyName')
-adminEmail = config.get('default', 'AdminEmail')
-primarySenderDomainToFilter = config.get('default', 'PrimarySenderDomainToFilter')
-aafo = config.get('default', 'AddressesAllowedForOutbound')
-aod = config.get('default', 'AllowedOutboundDomains')
-addressesAllowedForOutbound = aafo.split(',')
-allowedOutboundDomains = aod.split(',')
-bounceSender = config.get('default', 'BounceSender')
-logFile = config.get('default', 'LogFile')
-
-logging.basicConfig(level=logging.INFO, filename=logFile, filemode='a', format='%(asctime)s :: %(levelname)s :: %(message)s')
-
-server = CustomSMTPServer(('127.0.0.1', 10025), None)
-
-asyncore.loop()
+    handler = CustomSMTPHandler(config)
+    controller = Controller(handler, hostname='127.0.0.1', port=10025)
+    controller.start()
+    print("SMTP server running. Press Ctrl+C to stop.")
+    try:
+        asyncio.get_event_loop().run_forever()
+    except KeyboardInterrupt:
+        controller.stop()
+        print("SMTP server stopped.")
